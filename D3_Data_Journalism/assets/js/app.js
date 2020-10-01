7c457e2fadb1bbb89601ac194ddd2c20c2ad9d85
Allison Howe <allisonhowe77@gmail.com>
2020-09-30 22:45:52 -0400

    update

--- D3_Data_Journalism/assets/js/app.js
+++ D3_Data_Journalism/assets/js/app.js
@@ -1,1 +1,39 @@
-// @TODO: YOUR CODE HERE!
+// Step 1: Set up our chart
+var svgWidth = 960;
+var svgHeight = 500;
+
+var margin = {
+  top: 20,
+  right: 40,
+  bottom: 60,
+  left: 50
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+// Step 2: Create an SVG wrapper, append an SVG group that will hold our chart,
+// and shift the latter by left and top margins.
+var svg = d3
+  .select("body")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+d3.csv("../data/data.csv").then(function(csvData) {
+  csvData.forEach(function(data) {
+    data.obesity = +data.obesity;
+    data.income = +data.income;
+  });
+
+  var xLinearScale = d3.scaleLinear()
+    .domain([d3.min(csvData, d => d.income), d3.max(csvData, d => d.income)])
+    .range([0, width]);
+
+  var yLinearScale = d3.scaleLinear()
+    .domain([d3.min(csvData, d => d.obesity), d3.max(csvData, d => d.obesity)])
+    .range([height, 0]);
+})
